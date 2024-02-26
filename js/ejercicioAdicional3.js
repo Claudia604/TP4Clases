c1cfe51f22bf38c39ac66cb2e183b0c081f553c9
Claudia Torres <alet24665@gmail.com>
2024-02-26 15:50:14 -0300

    Se agrega ejercicio adicional 3

---
+++ js/ejercicioAdicional3.js
@@ -0,0 +1,65 @@
+/* 3- Catálogo de biblioteca: Tienes un array que almacena títulos de libros en una biblioteca. Debes 
+crear funciones que permitan a los usuarios buscar libros por título, verificar si un libro específico
+está disponible para préstamo y llevar un registro de la disponibilidad de los libros. */
+
+// const catalogoBiblioteca = [
+//     "Harry Potter y la piedra filosofal",
+//     "Odisea",
+//     "El principito",
+//     "Orgullo y prejuicio",
+//     "Hamlet",
+//   ];
+
+//   function buscarLibro(titulo) {
+//     const libroEncontrado = catalogoBiblioteca.find(
+//       (libro) => libro.toLowerCase() === titulo.toLowerCase()
+//     );
+//     return libroEncontrado || "Libro no encontrado";
+//   }
+
+//   function disponibilidad(titulo) {
+//     const libroEncontrado = buscarLibro(titulo);
+//     return libroEncontrado !== "Libro no encontrado";
+//   }
+
+//   document.write("Catálogo de la biblioteca:<br>");
+//   catalogoBiblioteca.forEach((libro) => document.write(`${libro}<br>`));
+
+const catalogoBiblioteca = [
+  { titulo: "Harry Potter y la piedra filosofal", disponible: true },
+  { titulo: "Odisea", disponible: true },
+  { titulo: "El principito", disponible: false },
+  { titulo: "Orgullo y prejuicio", disponible: true },
+  { titulo: "Hamlet", disponible: false },
+];
+
+function buscarLibro(titulo) {
+  const libroEncontrado = catalogoBiblioteca.find(
+    (libro) => libro.titulo.toLowerCase() === titulo.toLowerCase()
+  );
+  return libroEncontrado || "Libro no encontrado";
+}
+
+function disponibilidad(titulo) {
+  const libroEncontrado = buscarLibro(titulo);
+  if (libroEncontrado !== "Libro no encontrado") {
+    return libroEncontrado.disponible ? "Libro disponible" : "Libro prestado";
+  }
+  return libroEncontrado;
+}
+
+function prestarLibro(titulo) {
+  const libroEncontrado = catalogoBiblioteca.find(
+    (libro) => libro.titulo.toLowerCase() === titulo.toLowerCase()
+  );
+  if (libroEncontrado && libroEncontrado.disponible) {
+    libroEncontrado.disponible = false;
+    return `El libro "${titulo}" esta disponible.`;
+  } else if (libroEncontrado) {
+    return `El libro "${titulo}" no esta disponible.`;
+  } else {
+    return "Libro no encontrado";
+  }
+}
+
+document.write(prestarLibro("Harry Potter y la piedra filosofal"));
